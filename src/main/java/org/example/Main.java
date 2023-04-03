ffbf55b433c2e4d3f0288a9dd63253a8c1fb5a96
Yawkar <yawkarpov@gmail.com>
2023-04-03 16:21:54 +0300

    fix: change counting logic according to the actual task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -26,32 +28,32 @@ public class Main {
         if (outputFile.createNewFile()) {
             System.out.printf("File '%s' doesn't exist, created the empty one%n", outputFile.getAbsolutePath());
         }
-        var results = countUpperAndLowerCharacters(inputFile);
-        writeResultsToFile(results[0], results[1], outputFile);
+        var results = countLatinLetters(inputFile);
+        writeResultsToFile(results, outputFile);
     }
 
     private static String promptFilename(String fileDescription) {
-        System.out.print(String.format("Please, enter the %s filename: ", fileDescription));
+        System.out.printf("Please, enter the %s filename: ", fileDescription);
         return scanner.nextLine();
     }
 
     /**
-     * Counts lower- and upper-cased latin characters
-     * @param file
-     * @return array with 2 numbers: [lower, upper]
+     * Returns a map in which keys are characters and values are numbers of occurrences
+     * @param file A file to perform counting on
+     * @return An aforementioned map
      * @throws IOException in cases if the file doesn't exist or stream was closed
      */
-    private static int[] countUpperAndLowerCharacters(File file) throws IOException {
-        int[] counts = {0, 0};
+    private static Map<Character, Integer> countLatinLetters(File file) throws IOException {
+        Map<Character, Integer> counts = new HashMap<>();
         try (var bis = new BufferedInputStream(new FileInputStream(file))) {
             int character;
             while ((character = bis.read()) != -1) {
                 if (Character.UnicodeBlock.of(character).equals(Character.UnicodeBlock.BASIC_LATIN)
                     && Character.isAlphabetic(character)) {
                     if (Character.isLowerCase(character)) {
-                        ++counts[0];
+                        counts.put((char) character, counts.getOrDefault((char) character, 0) + 1);
                     } else {
-                        ++counts[1];
+                        counts.put((char) character, counts.getOrDefault((char) character, 0) + 1);
                     }
                 }
             }
@@ -59,11 +61,28 @@ public class Main {
         return counts;
     }
 
-    private static void writeResultsToFile(int lowerCased, int upperCased, File outputFile) throws IOException {
+    private static void writeResultsToFile(Map<Character, Integer> results, File outputFile) throws IOException {
         try (var fw = new FileWriter(outputFile, false)) {
             fw.write("Counting results:" + System.lineSeparator());
-            fw.write(String.format("Lower-cased: %d%n", lowerCased));
-            fw.write(String.format("Upper-cased: %d%n", upperCased));
+            for (var entry : results.entrySet()) {
+                fw.write(String.format("%c: %d%n", entry.getKey(), entry.getValue()));
+            }
+            fw.write(String.format("Total capital latin letters: %d%n", results.entrySet().stream()
+                    .filter(e -> Character.isUpperCase(e.getKey()))
+                    .mapToInt(Map.Entry::getValue)
+                    .sum()));
+            fw.write(String.format("Total lowercase latin letters: %d%n", results.entrySet().stream()
+                    .filter(e -> Character.isLowerCase(e.getKey()))
+                    .mapToInt(Map.Entry::getValue)
+                    .sum()));
+            fw.write(String.format("Total distinct capital latin letters: %d%n", results.keySet().stream()
+                    .filter(Character::isUpperCase)
+                    .distinct()
+                    .count()));
+            fw.write(String.format("Total distinct lowercase latin letters: %d%n", results.keySet().stream()
+                    .filter(Character::isLowerCase)
+                    .distinct()
+                    .count()));
         }
     }
 }
